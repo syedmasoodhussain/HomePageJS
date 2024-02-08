266cc3ceb86d05ea0ce923ce20a5898b9ae13dde
syed masood hussain <syedmasoodhussain551@gmail.com>
2024-02-07 21:05:35 -0500

    Landing Page JS

---
+++ script.js
@@ -0,0 +1,17 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+
+dayNight.addEventListener("click", () => {
+    banner.classList.toggle("night");
+    // Toggle icon visibility
+    dayNight.querySelectorAll("ion-icon").forEach(icon => icon.classList.toggle("hidden"));
+});
+
+
+let typingEffect = new Typed("#text", {
+    strings: ["Home", "About", "Contact", "work"], // Array of strings to display
+    loop: true, // Whether to loop through the strings continuously
+    typeSpeed: 100, // Typing speed in milliseconds
+    backSpeed: 50, // Backspacing speed in milliseconds
+    backDelay: 1000 // Delay before starting to backspace in milliseconds
+});
