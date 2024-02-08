dc5f1d172d9860855c92fb6b86751dc006abd8c5
syed masood hussain <syedmasoodhussain551@gmail.com>
2024-02-08 00:00:34 -0500

    added form

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ let typingEffect = new Typed("#text", {
     backSpeed: 50, // Backspacing speed in milliseconds
     backDelay: 1000 // Delay before starting to backspace in milliseconds
 });
+
+
+let typingEffects = new Typed("#texts", {
+    strings: ["Contact", "Me"], // Array of strings to display
+    loop: true, // Whether to loop through the strings continuously
+    typeSpeed: 100, // Typing speed in milliseconds
+    backSpeed: 50, // Backspacing speed in milliseconds
+    backDelay: 1000 // Delay before starting to backspace in milliseconds
+});
